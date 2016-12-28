3a273e908d1296e14d37ba569b818814d7060fd7
Louie Adamian <LouieAdamian@users.noreply.github.com>
2016-12-28 03:13:00 -0500

    adds more texts breaks Github link

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const CleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
   return gulp.src('/www/scss/style.scss')
@@ -10,4 +11,10 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('/scss/styles.scss', ['scss'])
 });
-gulp.task('default',['sass','sass:watch'])
+gulp.task('minify-css', function() {
+  return gulp.src('css/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default',['sass','sass:watch','minify-css'])
